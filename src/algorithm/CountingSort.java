fc2052b6bd1f36927720962865d80bb6ba935600
Zikrulla Yashinov <zikrullayashinov173@gmail.com>
2024-11-05 21:35:33 +0500

    Counting Sort

---
+++ src/algorithm/CountingSort.java
@@ -0,0 +1,33 @@
+package algorithm;
+
+import java.util.Arrays;
+
+public class CountingSort {
+    public int[] sort(int[] arr) {
+        int min = arr[0];
+        int max = arr[0];
+        for (int val : arr) {
+            if (val < min) min = val;
+            if (val > max) max = val;
+        }
+
+        int[] counts = new int[max - min + 1];
+        for (int value : arr) counts[value - min]++;
+
+        int[] sorted = new int[arr.length];
+        int pos = 0;
+        for (int i = 0; i < counts.length; i++)
+            for (int k = 0; k < counts[i]; k++)
+                sorted[pos++] = i + min;
+        return sorted;
+    }
+}
+
+class CountingSortTest {
+    public static void main(String[] args) {
+        CountingSort countingSort = new CountingSort();
+        int[] arr = new int[]{5, 4, 3, 2, 1, 83, 10};
+        int[] sorted = countingSort.sort(arr);
+        System.out.println(Arrays.toString(sorted));
+    }
+}
