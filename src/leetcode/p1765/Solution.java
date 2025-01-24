87556d24a6f08945e3155d524c3a7ac829c46d8e
Zikrulla <zikrullayashinov173@gmail.com>
2025-01-24 15:21:38 +0500

    Update p1765

--- src/leetcode/p1765/Solution.java
+++ src/leetcode/p1765/Solution.java
@@ -5,28 +5,28 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class Solution {
+
     public int[][] highestPeak(int[][] isWater) {
         int n = isWater.length;
         int m = isWater[0].length;
         int[][] res = new int[n][m];
+        int[] directions = {0, 1, 0, -1, 0};
+        int maxValue = m + n;
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                res[i][j] = isWater[i][j] == 1 ? 0 : maxValue;
+
+        boolean[][] visited = new boolean[n][m];
+        Queue<int[]> queue = new LinkedList<>();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (isWater[i][j] == 1) {
-                    bfs(isWater, res, i, j, n, m);
+                    visited[i][j] = true;
+                    queue.offer(new int[]{i, j});
                 }
             }
         }
-        return res;
-    }
-
-    int[] directions = {0, 1, 0, -1, 0};
-
-    private void bfs(int[][] isWater, int[][] res, int x, int y, int n, int m) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{x, y});
-
-        boolean[][] visited = new boolean[n][m];
-        visited[x][y] = true;
 
         while (!queue.isEmpty()) {
             int[] poll = queue.poll();
@@ -38,17 +38,19 @@ class Solution {
                         && newX < n && newY < m
                         && isWater[newX][newY] == 0
                         && !visited[newX][newY]
-                        && res[newX][newY] > res[x][y] + 1) {
+                        && res[newX][newY] > res[i][j] + 1) {
                     res[newX][newY] = res[i][j] + 1;
                     queue.offer(new int[]{newX, newY});
                     visited[newX][newY] = true;
                 }
             }
         }
+
+        return res;
     }
 
     public static void main(String[] args) {
-        int[][] isWater = new int[][]{{0, 1}, {0, 0}};
+        int[][] isWater = new int[][]{{0, 0, 1}, {1, 0, 0}, {0, 0, 0}};
         System.out.println(Arrays.deepToString(new Solution().highestPeak(isWater)));
     }
 }
