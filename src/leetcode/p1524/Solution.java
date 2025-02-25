6f38f4ebababe0a9a5ccd90d705cf43461aced0b
Zikrulla <zikrullayashinov173@gmail.com>
2025-02-25 12:01:11 +0500

    Add p1524

---
+++ src/leetcode/p1524/Solution.java
@@ -0,0 +1,16 @@
+package leetcode.p1524;
+
+class Solution {
+    public int numOfSubarrays(int[] arr) {
+        int n = arr.length;
+        int sum = 0;
+        long countOdd = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+            countOdd += sum % 2;
+        }
+
+        return (int) ((countOdd * (n - countOdd + 1)) % 1_000_000_007L);
+    }
+}
