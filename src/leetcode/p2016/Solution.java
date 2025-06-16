75bbd2ecfc97e49c067d28626b3ffc2b58af289f
Zikrulla <zikrullayashinov173@gmail.com>
2025-06-16 08:39:31 +0500

    Add p2016 (Easy)

---
+++ src/leetcode/p2016/Solution.java
@@ -0,0 +1,24 @@
+package leetcode.p2016;
+
+class Solution {
+    public int maximumDifference(int[] nums) {
+        int maxDifference = -1;
+        int n = nums.length;
+        int left = 0, right = 1;
+
+        while (left < right && right < n) {
+            int dif = nums[right] - nums[left];
+
+            if (dif < 0) {
+                left = right;
+            }
+            right++;
+
+            if (dif > maxDifference) {
+                maxDifference = dif;
+            }
+        }
+
+        return maxDifference > 0 ? maxDifference : -1;
+    }
+}
