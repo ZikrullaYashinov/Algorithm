630aa9f61125cbf9d8941d4371c576ce6d87ad7f
Zikrulla <zikrullayashinov173@gmail.com>
2025-07-14 09:06:03 +0500

    Add p1229 (Easy)

---
+++ src/leetcode/p3440/SolutionKotlin.kt
@@ -0,0 +1,37 @@
+//package leetcode.p3440
+//
+//class SolutionKotlin {
+//    fun maxFreeTime(eventTime: Int, startTime: IntArray, endTime: IntArray): Int {
+//         var time = 0
+//        val n = startTime.size;
+//
+//        val arr = IntArray(n + 1)
+//        val arrItems = IntArray(n)
+//
+//        for (i in 0 until n) {
+//            arr[i] = startTime[i] - time
+//            arrItems[i] = endTime[i] - startTime[i]
+//            time = endTime[i]
+//        }
+//
+//        arr[n] = eventTime - endTime[n - 1]
+//
+//        for (i in 0 until n) {
+//
+//        }
+//
+//        var sum = 0
+//        for (i in 0 until k) {
+//            sum += arr[i]
+//        }
+//
+//        var maxSum = 0
+//        for (i in k until n + 1) {
+//            sum += arr[i]
+//            if (sum > maxSum) maxSum = sum
+//            sum -= arr[i - k]
+//        }
+//
+//        return maxSum
+//    }
+//}
