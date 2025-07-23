f60d8b2fac6d6d7b48fe07b5c1ccb652094bbbb0
Zikrulla <zikrullayashinov173@gmail.com>
2025-07-23 09:34:59 +0500

    Add p1717 (Medium)

---
+++ src/leetcode/p1695/Solution.java
@@ -0,0 +1,31 @@
+package leetcode.p1695;
+
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        int[] arr = new int[10001];
+
+        int left = 0, right = 0;
+        int sum = 0, max = 0;
+        int n = nums.length;
+        while (right < n) {
+            int num = nums[right];
+            if (arr[num] == 0) {
+                arr[num]++;
+                sum += num;
+                right++;
+            } else {
+                num = nums[left];
+                arr[num]--;
+                sum -= num;
+                left++;
+            }
+            if (sum > max) max = sum;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.maximumUniqueSubarray(new int[]{4, 2, 4, 5, 6}));
+    }
+}
---
+++ src/leetcode/p1957/Solution.kt
@@ -0,0 +1,28 @@
+package leetcode.p1957
+
+class Solution {
+    fun makeFancyString(s: String): String {
+        val charArray = s.toCharArray()
+        val sb = StringBuilder()
+
+        var last = charArray[0]
+        var count = 1
+        sb.append(last)
+
+        for (i in 1..charArray.size - 1) {
+            if (last != charArray[i]) {
+                count = 1
+                last = charArray[i]
+            } else
+                count++
+            if (count < 3)
+                sb.append(last)
+        }
+
+        return sb.toString()
+    }
+}
+
+fun main() {
+    println(Solution().makeFancyString("leeetcode"))
+}
