d7f66c96ff9e3b96d0d6fa049fc9fe60402b40ce
Zikrulla <zikrullayashinov173@gmail.com>
2025-11-13 09:32:32 +0500

    Adds p3228 LeetCode solution.

---
+++ src/problems/leetcode/p3228/Main.java
@@ -0,0 +1,28 @@
+package problems.leetcode.p3228;
+
+class Solution {
+    public int maxOperations(String s) {
+        int sum = 0;
+        int temp = 0;
+        int count = 0;
+        boolean flag = true;
+        for (char c : s.toCharArray()) {
+            if (c == '1') {
+                count++;
+                flag = true;
+            } else if (flag) {
+                temp += count;
+                sum += temp;
+                flag = false;
+                count = 0;
+            }
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.maxOperations("1001101"));
+    }
+}
---
+++ src/problems/leetcode/p3318/Solution.java
@@ -0,0 +1,21 @@
+//package problems.leetcode.p3318;
+//
+//import java.util.Arrays;
+//
+//class Solution {
+//    public int[] findXSum(int[] nums, int k, int x) {
+//        int[] arr = new int[51];
+//        for (int i = 0; i < k; i++) {
+//            arr[nums[i]]++;
+//        }
+//
+//        Arrays.sort(arr);
+//        int max1 = arr[50];
+//        int max2 = arr[49];
+//
+//        int sum = 0;
+//        for (int i = 0; i < nums.length; i++) {
+//
+//        }
+//    }
+//}
